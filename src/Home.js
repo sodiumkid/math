698b7493123c9dca87862dd1acc8d3331b08eb09
Daniel Na <element11sodium@gmail.com>
2020-05-11 12:18:46 -0700

    juarez frenzy

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './Home.css';
 import './bootstrap.min.css';
+import './Home.css';
 import {Link} from "react-router-dom";
+
+import animate from "animate.css";
+
 function Home() {
   return (
     <div className="Home">
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <Link class="navbar-brand" to="/">MATH FINAL</Link>
+      <img class="App-logo" src="https://res.cloudinary.com/drferrel/image/upload/v1589223893/misc/72061539_530606401005543_5876809008156770304_n_rusckn.png"/>
+      <Link class="navbar-brand" to="/" >MATH FINAL</Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -34,6 +38,8 @@ function Home() {
         <div class='quizzer'>
           <h1>Welcome!</h1>
           <p>go to the quiz tab</p>
+          <img class="animate__animated animate__delay-1s animate__infinite animate__wobble" src="https://res.cloudinary.com/drferrel/image/upload/v1589223890/misc/IMG_0591_uxnc2g.png"/>
+          <br/>
         </div>
       </body>
     </div>
