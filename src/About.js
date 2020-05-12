9b1a93204fff5e051d0b66a64be87522ea1af251
Daniel Na <element11sodium@gmail.com>
2020-05-11 21:13:19 -0700

    memes

--- src/About.js
+++ src/About.js
@@ -1,44 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './Home.css';
 import './bootstrap.min.css';
+import './Resources.css';
 import {Link} from "react-router-dom";
-
-function About() {
-  return (
-    <div className="About">
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <img class="App-logo" src="https://res.cloudinary.com/drferrel/image/upload/v1589223893/misc/72061539_530606401005543_5876809008156770304_n_rusckn.png"/>
-      <Link class="navbar-brand" to="/">MATH FINAL</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <Link class="nav-link" to="/resources">Resources</Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/quiz">Quiz</Link>
-          </li>
-          <li class="nav-item active">
-            <Link class="nav-link" to="/about">About <span class="sr-only">(current)</span></Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+import snekfetch from "snekfetch";
 
 
 
-      <body>
-        <div class='quizzer'>
-          <p> bruh why would you click on this page i havent made it yet get out of here and stop wasting both of our times</p>
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {image: "https://miro.medium.com/max/1080/0*DqHGYPBA-ANwsma2.gif"};
+  }
+
+  componentDidMount() {
+    this.getReddit();
+  }
+
+  getReddit = async() => {
+    const url = 'https://www.reddit.com/r/memes.json?sort=top&t=week';
+    try {
+        const { body } = await snekfetch
+            .get(url)
+            .query({ limit: 800 });
+        const allowed = body.data.children
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        console.log(allowed[randomnumber].data.url);
+        this.setState({
+          image : allowed[randomnumber].data.url
+        });
+    } catch (err) {
+        return console.log(err);
+    }
+  }
+  render() {
+    return (
+      <div className="About">
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <img class="App-logo" src="https://res.cloudinary.com/drferrel/image/upload/v1589223893/misc/72061539_530606401005543_5876809008156770304_n_rusckn.png"/>
+        <Link class="navbar-brand" to="/">MATH FINAL</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <Link class="nav-link" to="/resources">Resources</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/quiz">Quiz</Link>
+            </li>
+            <li class="nav-item active">
+              <Link class="nav-link" to="/about">About <span class="sr-only">(current)</span></Link>
+            </li>
+          </ul>
         </div>
-      </body>
-    </div>
-  );
+      </nav>
+
+
+
+        <body>
+          <div class='container'>
+            <h1> About </h1>
+            <h3> I don't know what to write so here's a randomly scraped image from r/memes</h3>
+            <br />
+            <img class="meme" src={this.state.image} />
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/Content.js
+++ src/Content.js
@@ -3,17 +3,31 @@ export const content = {
     {
       "index" : 1311,
       "Title" : "13.1.1 - Limits and which graphs are faster on going to infinity",
-      "Content" : "Some graphs are faster than others. These are determined by the term with the highest degree, because when it goes to infinity all the other terms become irrelevant. Because of this, when considering which terms go towards infinity faster we can only consider the term with the highest degree."
+      "Content" : "Some graphs are faster than others. These are determined by the term with the highest degree, because when it goes to infinity all the other terms become irrelevant. Because of this, when considering which terms go towards infinity faster we can only consider the term with the highest degree.",
+      "Files" : [
+        {
+          Number : "1.1.1",
+          Link: "https://balakla.com"
+        }
+      ],
+      "Mistakes" : "",
+      "Videos" : [
+        {
+          Title : "Khan Academy",
+          Link: "https://khanvideo.com"
+        }
+      ],
+      "World" : ""
     },
     {
       "index" : 1312,
       "Title" : "13.1.2 - Finding limits for polynomial fractions",
-      "Content" : "There are multiple ways to find limits to infinity. One way is to ignore all digits except for the most significant digit of the numerator and the denominator. This is because as the value for x increases, the other less significant digits become less and less important. From this point, you can follow a few simple rules: -If the power of the numerator is larger than the power of the denominator, x∞f(x)=∞. -If the power of the numerator is smaller than the power of the denominator, x∞f(x)=0. -If the power of the numerator and denominator are equal, x∞f(x)is equal the the coefficient of the highest power digit of the numerator divided by the coefficient of the highest power digit of denominator"
+      "Content" : "There are multiple ways to find limits to infinity. One way is to ignore all digits except for the most significant digit of the numerator and the denominator. This is because as the value for x increases, the other less significant digits become less and less important. From this point, you can follow a few simple rules: -If the power of the numerator is larger than the power of the denominator, x∞f(x)=∞. -If the power of the numerator is smaller than the power of the denominator, x∞f(x)=0. -If the power of the numerator and denominator are equal, x∞f(x)is equal the the coefficient of the highest power digit of the numerator divided by the coefficient of the highest power digit of denominator",
     },
     {
       "index" : 1313,
       "Title" : "13.1.3 - Finding limits for undefined parts of functions",
-      "Content" : "When direct substitution of x=a in xa f(x)leads to 00, we say that this result is inconclusive. We need to investigate further whether the numerator or the denominator approaches zero faster and/or if the terms leading to the indeterminate form can be eliminated. Example: find x -1x2-1x+1 Solution: Direct substitution of x=-1 will lead to the so called indeterminate form00. Factoring of the numerator and reducing will be the way to solve: x -1x2-1x+1= x -1(x+1)(x-1)x+1=x -1x-1= -1 -1 = -2"
+      "Content" : "When direct substitution of x=a in xa f(x)leads to 00, we say that this result is inconclusive. We need to investigate further whether the numerator or the denominator approaches zero faster and/or if the terms leading to the indeterminate form can be eliminated. Example: find x -1x2-1x+1 Solution: Direct substitution of x=-1 will lead to the so called indeterminate form00. Factoring of the numerator and reducing will be the way to solve: x -1x2-1x+1= x -1(x+1)(x-1)x+1=x -1x-1= -1 -1 = -2",
     },
     {
       "index" : 1314,
