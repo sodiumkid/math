81ba6cab06434a70e8a4acb84176e98216f71038
Daniel Na <element11sodium@gmail.com>
2020-05-21 21:06:24 -0700

    new quiz

--- src/questions.js
+++ src/questions.js
@@ -1,111 +1,62 @@
 export const questions = {
-  "quizTitle": "The Ultimate Test",
-  "quizSynopsis": "Prove your worth as a human being.",
+  "quizTitle": "Practice Quiz",
+  "quizSynopsis": "daniel fischel is dumbo",
   "questions": [
     {
-      "question": "9+10",
+      "question": "<img src='https://res.cloudinary.com/drferrel/image/upload/v1590118797/misc/juarez/Annotation_2020-05-21_203937_lkiibk.png'>",
       "questionType": "text",
       "answers": [
-        "1",
-        "19",
-        "420",
-        "21"
+        "4",
+        "3",
+        "2",
+        "0.523"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "point": "20"
+    },
+    {
+      "question": "<img src='https://res.cloudinary.com/drferrel/image/upload/v1590119074/misc/juarez/Annotation_2020-05-21_204420_emyiw1.png'/>",
+      "questionType": "text",
+      "answers": [
+        "10",
+        "4.53",
+        "9.45",
+        "9.998"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "point": "20"
+    },
+    {
+      "question": "<img src='https://res.cloudinary.com/drferrel/image/upload/v1590119461/misc/juarez/Annotation_2020-05-21_204927_kaf3gq.png'/>",
+      "questionType": "text",
+      "answers": [
+        "5",
+        "2",
+        "4",
+        "11"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "point": "20"
+    },
+    {
+      "question": "<img src='https://res.cloudinary.com/drferrel/image/upload/v1590119980/misc/juarez/Annotation_2020-05-21_205919_upkpqh.png'/>",
+      "questionType": "text",
+      "answers": [
+        "4",
+        "5",
+        "0.5",
+        "0.33"
       ],
       "correctAnswer": "4",
-      "messageForCorrectAnswer": "Good. You passed the first one.",
-      "messageForIncorrectAnswer": "Are you serious",
-      "point": "1"
-    },
-    {
-      "question": "Brian is black.",
-      "questionType": "text",
-      "answers": [
-        "True",
-        "False"
-      ],
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "Good. You passed the second one.",
-      "messageForIncorrectAnswer": "Bruh do you have eyes",
-      "point": "1"
-    },
-    {
-      "question": "What is the meaning of life?",
-      "questionType": "text",
-      "answers": [
-        "\"the meaning of life\"",
-        "42",
-        "Jesus Christ",
-        "Veganism"
-      ],
-      "correctAnswer": "3",
-      "messageForCorrectAnswer": "One name is higher\nOne name is stronger\nThan any grave, than any throne\nChrist exalted over all\nThe only Saviour, Jesus Messiah\nTo You alone our praise belongs\nChrist exalted over all",
-      "messageForIncorrectAnswer": "You godless piece of garbage",
-      "point": "2"
-    },
-    {
-      "question": "What is the greatest show?",
-      "questionType": "text",
-      "answers": [
-        "VeggieTales",
-        "Rick and Morty",
-        "Parks and Recreation",
-        "The Greatest Showman"
-      ],
-      "correctAnswer": "2",
-      "messageForIncorrectAnswer": "You uncultured swine",
-      "messageForCorrectAnswer": "good",
-      "explanation": "ur taste is bad"
-    },
-    {
-      "questionType": "text",
-      "answers": [
-        "Yes",
-        "No"
-      ],
-      "question": "Are you depressed?",
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "welcome to the club",
-      "messageForIncorrectAnswer": "well be depressed you little dumb",
-      "point": "2"
-    },
-    {
-      "question": "Who is the best politician?",
-      "questionType": "text",
-      "answers": [
-        "Ben Shapiro",
-        "PewDiePie",
-        "Donald Trump",
-        "Jesus Christ"
-      ],
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "facts dont blah blah blah",
-      "messageForIncorrectAnswer": "do u even politics",
-      "point": "1"
-    },
-    {
-      "question": "What is the best anime?",
-      "questionType": "text",
-      "answers": [
-        "Attack on Titan",
-        "Cory in the House",
-        "Pokemon",
-        "I'm not a weeb"
-      ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "yeee",
-      "messageForIncorrectAnswer": "u weeb",
-      "point": "1"
-    },
-    {
-      "questionType": "photo",
-      "answers": [
-        "https://vignette.wikia.nocookie.net/rickandmorty/images/3/37/Mr_poopy_butthole.png/revision/latest?cb=20150819161234",
-        "https://vignette.wikia.nocookie.net/rickandmorty/images/c/c8/Noob_Noob.png/revision/latest?cb=20180614022930"
-      ],
-      "question": "Which is MR. PBH",
-      "messageForCorrectAnswer": "duh",
-      "messageForIncorrectAnswer": "boi",
-      "correctAnswer": "1"
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "point": "20"
     }
   ]
 }
